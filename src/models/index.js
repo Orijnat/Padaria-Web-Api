8ef3c0cd6df1a338433c0322f0eb8952db7a5f4e
Orijnat <cledivanleao@gmail.com>
2026-09-14 20:20:40 -0300

    Feat: criado os models e o .gitignore

---
+++ src/models/index.js
@@ -0,0 +1,13 @@
+import Cliente from "./ClienteModel.js";
+import Insumo from "./InsumoModel.js";
+import Produto from "./ProdutoModel.js";
+import ItemReceita from "./ItemReceitaModel.js";
+
+(async () => {
+    // await Cliente.sync();
+    // await Insumo.sync();
+    // await Produto.sync();
+    // await ItemReceita.sync();
+})();
+
+export { Cliente, Insumo, Produto, ItemReceita };
